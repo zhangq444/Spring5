a43a4789766c19b92d2ca2416e196c9f43da946d
DESKTOP-CT4OS6H\grzha <grzhangqiang@163.com>
2022-05-29 12:02:01 +0800

    spring5

---
+++ bean/src/main/java/com/ithema/study/A01Application.java
@@ -0,0 +1,40 @@
+package com.ithema.study;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+/**
+ * Created by grzha on 2022/5/28.
+ */
+@SpringBootApplication
+@Slf4j
+public class A01Application {
+
+    public static void main(String[] args) throws Exception{
+        ConfigurableApplicationContext context = SpringApplication.run(A01Application.class, args);
+        System.out.println(context);
+
+        Field objects = DefaultSingletonBeanRegistry.class.getDeclaredField("singletonObjects");
+        objects.setAccessible(true);
+
+        ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+        Map<String,Object> map= (Map<String, Object>) objects.get(beanFactory);
+        map.forEach((k,v)->{
+            System.out.println(k+"--"+v);
+        });
+
+        String property = context.getEnvironment().getProperty("app.id");
+        System.out.println(property);
+
+
+
+    }
+
+}
---
+++ bean/src/main/java/com/ithema/study/controller/Component01.java
@@ -0,0 +1,19 @@
+package com.ithema.study.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by grzha on 2022/5/28.
+ */
+@Component
+@Slf4j
+@Data
+public class Component01 {
+
+    private static String name="Component01";
+
+
+
+}
